d509a09c1ac12a145858856155e5661e4223471c
KuwadaKohei <24120053@oic-ok.ac.jp>
2025-11-22 12:42:29 +0900

    ログアウトサーブレットを実装

--- Oic_IKZM_JHM/src/main/java/ikzm_jhm_servlet/LogoutServlet.java
+++ Oic_IKZM_JHM/src/main/java/ikzm_jhm_servlet/LogoutServlet.java
@@ -2,11 +2,13 @@ package ikzm_jhm_servlet;
 
 import java.io.IOException;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 /**
  * セッション破棄とログアウト処理
@@ -18,10 +20,13 @@ public class LogoutServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException {
 
-	}
-
-	protected void doPost(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException {
-
+		//セッションスコープを削除
+		HttpSession session = request.getSession();
+		session.invalidate();
+		
+		//ログアウト画面へ遷移
+		RequestDispatcher dispatcher = request.getRequestDispatcher("logout.jsp");
+		dispatcher.forward(request, response);
 	}
 
 }
