3cf5a6ae28956e949924856d3b9d1890f096a625
KuwadaKohei <24120053@oic-ok.ac.jp>
2025-11-24 17:27:07 +0900

    現時点でのメソッドを揃えた

--- Oic_IKZM_JHM/src/main/java/ikzm_jhm_dao/UserDAO.java
+++ Oic_IKZM_JHM/src/main/java/ikzm_jhm_dao/UserDAO.java
@@ -1,5 +1,7 @@
 package ikzm_jhm_dao;
 
+import java.util.ArrayList;
+
 import ikzm_jhm_dto.User;
 
 /*
@@ -8,19 +10,38 @@ import ikzm_jhm_dto.User;
  */
 public class UserDAO {
 
+	//ユーザーIDを使用してユーザーレコードを取得する
+	public User findById(int searchWordInt) {
+		// TODO 自動生成されたメソッド・スタブ
+		return null;
+	}
+
 	//GoogleアカウントIDを使用してユーザーレコードを取得する
 	public User findByGoogleAccountId(String targetAccountId) {
 		return null;
 	}
 
+	//メールアドレスを使用してユーザーリストを取得する
+	public ArrayList<User> findListByEmail(String email) {
+		// TODO 自動生成されたメソッド・スタブ
+		return null;
+	}
+
+	//ユーザー名を使用してユーザーリストを取得する
+	public ArrayList<User> findListByUsername(String email) {
+		// TODO 自動生成されたメソッド・スタブ
+		return null;
+	}
+
 	//新規ログインユーザーを登録し、ユーザーIDを返す
-	public int insertGoogleUser(String accountId, String email, String userType,String name, boolean isActive) {
+	public int insertGoogleUser(String accountId, String email, String userType, String name, boolean isActive) {
 		return 0;
 	}
 
 	//引数のユーザーIDのレコードに学科が登録されているか確認する
 	public boolean hasDepartment(int userId) {
-		
+		return false;
+
 	}
 
 }
