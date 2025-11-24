4063c364d138142457fb717ad9321a9d692c8d64
KuwadaKohei <24120053@oic-ok.ac.jp>
2025-11-24 21:07:23 +0900

    ユーザー制限機能の責務を他クラスへ譲渡、投稿削除実行機能の実装

--- Oic_IKZM_JHM/src/main/java/ikzm_jhm_action/PostManageAction.java
+++ Oic_IKZM_JHM/src/main/java/ikzm_jhm_action/PostManageAction.java
@@ -1,23 +1,28 @@
 package ikzm_jhm_action;
 
+import ikzm_jhm_dao.PostDAO;
+import ikzm_jhm_dto.Post;
+
 public class PostManageAction {
+	
 	//新規投稿処理（複数DAO呼び出し、トランザクション管理)
-	public int createReport(ReportForm form) {
+	public int createPost(ReportForm form) {
 		
 	}
-	
+
 	//投稿編集処理（旧データ削除・新規挿入含むトランザクション)
-	public void updateReport(ReportForm form) {
-		
+	public void updatePost(ReportForm form) {
+
 	}
-	
+
 	//投稿レコードを削除（トランザクション管理）
-	public void deleteReport(int postId) {
-		
+	public void deletePost(int postId) {
+		PostDAO postDAO = new PostDAO();
+		postDAO.deletePost(postId);
 	}
-	
-	//ユーザー資格停止処理
-	public void banUser(int targetId) {
-		
+
+	public Post searchPostAction() {
+		return null;
 	}
+
 }
