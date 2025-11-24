b070af3d951d56e929fe9ffd03ce3d6cf3ad680c
KuwadaKohei <24120053@oic-ok.ac.jp>
2025-11-24 16:14:08 +0900

    UserAction:

--- Oic_IKZM_JHM/src/main/java/ikzm_jhm_action/UserAction.java
+++ Oic_IKZM_JHM/src/main/java/ikzm_jhm_action/UserAction.java
@@ -1,28 +1,28 @@
 package ikzm_jhm_action;
 
+import java.util.ArrayList;
+
 import ikzm_jhm_dao.UserDAO;
 import ikzm_jhm_dto.User;
 
 public class UserAction {
 
 	/*
-	 * 検索文字列からユーザー情報を創作する
+	 * 検索文字列からユーザー情報を捜索する
 	 * 使用クラス：UserManageServlet
 	 */
-	public User SearchUser(String searchWord) {
+	public ArrayList<User> searchUser(String searchWord) {
 
-		User user = null;
 		UserDAO userDao = new UserDAO();
+		ArrayList<User> userList = new ArrayList<User>();
 
 		if (searchWord.startsWith("/")) {
-			user = userDao.findByEmail(searchWord);
-		}else if(searchWord.startsWith("@")) {
-			int searchWordInt = Integer.parseInt(searchWord);
-			user = userDao.findById(searchWordInt);
-		}else {
-			user = userDao.findByUsername(searchWord);
+			userList = userDao.findListByEmail(searchWord);
+		} else {
+			userList = userDao.findListByUsername(searchWord);
 		}
-		
-		return user;
+
+		return userList;
+
 	}
 }
