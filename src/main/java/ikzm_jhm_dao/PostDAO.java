49f9d237c3702cd1248a89010c3a2e917a94c291
misuzu <24120047@oic-ok.ac.jp>
2025-11-25 15:30:48 +0900

    PostDao途中

--- src/main/java/ikzm_jhm_dao/PostDAO.java
+++ src/main/java/ikzm_jhm_dao/PostDAO.java
@@ -1,14 +1,67 @@
+/***************************/
+/* メモの操作 **************/
+/***************************/
 package ikzm_jhm_dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.List;
 
 import ikzm_jhm_dto.Post;
+import ikzm_jhm_dto.PostExamSelection;
 
 public class PostDAO {
 
-	public Post searchPostById(String target) {
-		// TODO 自動生成されたメソッド・スタブ
-		return null;
+	public Post searchPostById(int target) throws Exception {
+		Connection con = ConnectionDAO.createConnection();
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		int postId = 0;
+		int userId = 0;
+		int departmentId = 0;
+		int methodId = 0;
+		String recruitmentNo = null;
+		String companyName = null;
+		String venueAddress = null;
+		LocalDate examDate = null;
+		int grade = 0;
+		boolean isAnonymous = false;
+		LocalDateTime createAt = null;
+		LocalDateTime updatedAt = null;
+		List<PostExamSelection> examSelection = null;
+		
+		try {
+			if(con != null) {
+				String sql = "SELECT * FROM POST WHERE POSTID = ?";
+				pstmt = con.prepareStatement(sql);
+				pstmt.setInt(1 , target);
+				rs = pstmt.executeQuery();
+				while(rs.next() == true) {
+					postId = rs.getInt(postId);
+					userId = postId = rs.getInt(userId);
+					departmentId = rs.getInt(departmentId);
+					methodId = rs.getInt(methodId);
+					recruitmentNo = rs.getString(recruitmentNo);
+					companyName = rs.getString(companyName);
+					venueAddress = rs.getString(venueAddress);
+					examDate = rs.getDate(examDate);
+				}
+			}
+			rs.close();
+			pstmt.close();
+			return post;
+		}catch(SQLException e) {
+			//情報取得に失敗(未規定)
+			throw new Exception("DB-2003");
+		}finally {
+			ConnectionDAO.closeConnection(con);
+		}
 	}
 	
 	//
