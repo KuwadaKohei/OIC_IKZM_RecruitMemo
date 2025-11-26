4f6728ddd5780c91e2ab2e94173357a2feb4f6db
misuzu <24120047@oic-ok.ac.jp>
2025-11-26 11:38:38 +0900

    PostDAOのsearchPostByExamDate()のメソッド途中

--- src/main/java/ikzm_jhm_dao/PostDAO.java
+++ src/main/java/ikzm_jhm_dao/PostDAO.java
@@ -8,6 +8,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
@@ -203,15 +204,168 @@ public class PostDAO {
 	}
 
 	//POSTテーブルからユーザーIDと求人番号を使用してPostリストを取得する
-	public List<Post> searchPostByRecruitmentNo(int userId, String substring) {
-		// TODO 自動生成されたメソッド・スタブ
-		return null;
+	public List<Post> searchPostByRecruitmentNo(int userId, String substring) throws Exception {
+		Connection con = ConnectionDAO.createConnection();
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		ArrayList<Post> list = new ArrayList<>();
+		
+		int postId = 0;
+		int departmentId = 0;
+		int methodId = 0;
+		String companyName = null;
+		String venueAddress = null;
+		LocalDate examDate = null;
+		int grade = 0;
+		boolean isAnonymous = false;
+		LocalDateTime createAt = null;
+		LocalDateTime updatedAt = null;
+		List<PostExamSelection> examSelection = null;
+		boolean isActive = false;
+		
+		try {
+			if(con != null) {
+				String sql = "SELECT * FROM POST WHERE USERID = ? AND RECURUITMENTNO = ?";
+				pstmt = con.prepareStatement(sql);
+				pstmt.setInt(1 , userId);
+				pstmt.setString(2 , substring);
+				rs = pstmt.executeQuery();
+				while(rs.next() == true) {
+					postId = rs.getInt("postId");
+					departmentId = rs.getInt("departmentId");
+					methodId = rs.getInt("methodId");
+					companyName = rs.getString("companyName");
+					venueAddress = rs.getString("venueAddress");
+					Date date_e = rs.getDate("examDate");
+					grade = rs.getInt("grade");
+					isAnonymous = rs.getBoolean("isAnonymous");
+					Date date_c = rs.getDate("createAt");
+					Date date_u = rs.getDate("updatedAt");
+					//List型のexamSelectionの処理//
+					isActive = rs.getBoolean("isActive");
+					
+					//localDate型に変換
+					examDate =  LocalDate.ofInstant(date_e.toInstant(), ZoneId.systemDefault());
+					
+					//localDateTime型に変換
+					createAt =  LocalDateTime.ofInstant(date_c.toInstant(), ZoneId.systemDefault());
+					updatedAt =  LocalDateTime.ofInstant(date_u.toInstant(), ZoneId.systemDefault());
+					
+					//Postインスタンスを生成
+					Post post = new Post(
+							postId,
+							userId,
+							departmentId,
+							methodId,
+							substring,
+							companyName,
+							venueAddress,
+							examDate,
+							grade,
+							isAnonymous,
+							createAt,
+							updatedAt,
+							examSelection,
+							isActive
+					);
+					
+					//listに格納
+					list.add(post);
+					
+				}
+			}
+			rs.close();
+			pstmt.close();
+			return list;
+		}catch(SQLException e) {
+			//情報取得に失敗(未規定)
+			throw new Exception("DB-2003");
+		}finally {
+			ConnectionDAO.closeConnection(con);
+		}
 	}
 
-	//POSTテーブルからユーザーIDと日付を使用してPostリストを取得する
-	public List<Post> searchPostByExamDate(int userId, LocalDate date) {
-		// TODO 自動生成されたメソッド・スタブ
-		return null;
+	//POSTテーブルからユーザーIDと試験日付を使用してPostリストを取得する
+	public List<Post> searchPostByExamDate(int userId, LocalDate examDate) throws Exception {
+		Connection con = ConnectionDAO.createConnection();
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		ArrayList<Post> list = new ArrayList<>();
+		
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+		Date date = simpleDateFormat.parse(examDate.toString());
+		
+		int postId = 0;
+		int departmentId = 0;
+		int methodId = 0;
+		String companyName = null;
+		String recruitmentNo = null;
+		String venueAddress = null;
+		int grade = 0;
+		boolean isAnonymous = false;
+		LocalDateTime createAt = null;
+		LocalDateTime updatedAt = null;
+		List<PostExamSelection> examSelection = null;
+		boolean isActive = false;
+		
+		try {
+			if(con != null) {
+				String sql = "SELECT * FROM POST WHERE USERID = ? AND RECURUITMENTNO = ?";
+				pstmt = con.prepareStatement(sql);
+				pstmt.setInt(1 , userId);
+				pstmt.setDate(2 , date);
+				rs = pstmt.executeQuery();
+				while(rs.next() == true) {
+					postId = rs.getInt("postId");
+					departmentId = rs.getInt("departmentId");
+					methodId = rs.getInt("methodId");
+					recruitmentNo = rs.getString("recruitmentNo");
+					companyName = rs.getString("companyName");
+					venueAddress = rs.getString("venueAddress");
+					grade = rs.getInt("grade");
+					isAnonymous = rs.getBoolean("isAnonymous");
+					Date date_c = rs.getDate("createAt");
+					Date date_u = rs.getDate("updatedAt");
+					//List型のexamSelectionの処理//
+					isActive = rs.getBoolean("isActive");
+					
+					
+					//localDateTime型に変換
+					createAt =  LocalDateTime.ofInstant(date_c.toInstant(), ZoneId.systemDefault());
+					updatedAt =  LocalDateTime.ofInstant(date_u.toInstant(), ZoneId.systemDefault());
+					
+					//Postインスタンスを生成
+					Post post = new Post(
+							postId,
+							userId,
+							departmentId,
+							methodId,
+							recruitmentNo,
+							companyName,
+							venueAddress,
+							date,
+							grade,
+							isAnonymous,
+							createAt,
+							updatedAt,
+							examSelection,
+							isActive
+					);
+					
+					//listに格納
+					list.add(post);
+					
+				}
+			}
+			rs.close();
+			pstmt.close();
+			return list;
+		}catch(SQLException e) {
+			//情報取得に失敗(未規定)
+			throw new Exception("DB-2003");
+		}finally {
+			ConnectionDAO.closeConnection(con);
+		}
 	}
 
 	public List<Post> searchPostByKeyword(int userId, String term) {
