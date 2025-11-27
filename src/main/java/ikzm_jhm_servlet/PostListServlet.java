0198f68edfc14f1ee680bc3132f11fac68e13387
KuwadaKohei <24120053@oic-ok.ac.jp>
2025-11-27 09:59:14 +0900

    投稿一覧表示(index)を行うサーブレットの実装

--- src/main/java/ikzm_jhm_servlet/PostListServlet.java
+++ src/main/java/ikzm_jhm_servlet/PostListServlet.java
@@ -2,11 +2,15 @@ package ikzm_jhm_servlet;
 
 import java.io.IOException;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+
+import ikzm_jhm_action.PostAction;
+import ikzm_jhm_viewmodel.SearchResultViewModel;
 
 /**
  * 一覧データの取得(ReportAction)
@@ -17,9 +21,27 @@ import jakarta.servlet.http.HttpServletResponse;
 public class PostListServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
+		String searchWord = request.getParameter("searchWord");
+
+		PostAction reportAction = new PostAction();
+		SearchResultViewModel viewModel;
+
+		if (searchWord != null && !searchWord.isBlank()) {
+
+			viewModel = reportAction.searchReports(searchWord);
+
+		} else {
+
+			viewModel = reportAction.getReportList();
+		}
+
+		request.setAttribute("postList", viewModel);
+
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/index.jsp");
+		dispatcher.forward(request, response);
 	}
 
 }
