1e5a623bd9817dd2556132e701ee368f7ebf47d4
KuwadaKohei <24120053@oic-ok.ac.jp>
2025-11-27 14:34:47 +0900

    selectionDAOの作成(未実装)

---
+++ src/main/java/ikzm_jhm_action/SelectionDAO.java
@@ -0,0 +1,14 @@
+package ikzm_jhm_action;
+
+import java.util.List;
+
+import ikzm_jhm_dto.PostExamSelection;
+
+public class SelectionDAO {
+
+	public void insertList(List<PostExamSelection> selections) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+	}
+	
+}
