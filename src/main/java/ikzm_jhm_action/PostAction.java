61fc192e48c80850f588ee8625faf58e50d68034
KuwadaKohei <24120053@oic-ok.ac.jp>
2025-11-27 10:31:28 +0900

    PostListServletが利用するActionメソッドを実装

--- src/main/java/ikzm_jhm_action/PostAction.java
+++ src/main/java/ikzm_jhm_action/PostAction.java
@@ -13,21 +13,55 @@ import ikzm_jhm_viewmodel.SearchResultViewModel;
 public class PostAction {
 
 	//一覧/検索リスト取得、ViewModel変換
-	public SearchResultViewModel getReportList() {
+	public SearchResultViewModel getPostList() {
 
-	}
+		PostDAO dao = new PostDAO();
 
-	//投稿の詳細データ取得、ViewModel変換、匿名性処理
-	public PostReportViewModel getReportDetails(int postId) {
-
-	}
-
-	public List<PostReportViewModel> getMyReportList(int userId) {
-
+		List<Post> posts = dao.findAll();
+		return ModelConverter.toSearchResultViewModel(posts);
 	}
 
 	//ユーザーから受け取った検索情報をもとに必要なDAOを呼び出す
-	public SearchResultViewModel searchReports(String word) {
+	public SearchResultViewModel searchPosts(String searchWord) {
+		PostDAO dao = new PostDAO();
+		List<Post> posts = null;
+
+		if (searchWord == null || searchWord.isBlank()) {
+			//全件取得
+			return getPostList();
+		}
+
+		//検索文字列から空白文字を取り除く
+		String term = searchWord.trim();
+
+		if (term.startsWith("#")) {
+			// #求人番号検索
+			posts = dao.searchPostByRecruitmentNo(term.substring(1));
+
+		} else if(term.startsWith("@")){
+			//日付検索
+			try {
+				String dateStr = term.substring(1);
+				LocalDate date = LocalDate.parse(dateStr, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+				posts = dao.searchPostByExamDate(date);
+			}catch(Exception e) {
+				
+			}
+			
+		}else {
+			//通常検索
+			posts = dao.searchPostByKeyword(term);
+			
+		}
+		
+		return ModelConverter.toSearchResultViewModel(posts);
+	}
+
+	//投稿の詳細データ取得、ViewModel変換、匿名性処理
+	public PostReportViewModel getPostDetails(int postId) {
+	}
+
+	public List<PostReportViewModel> getMyPostList(int userId) {
 
 	}
 
@@ -37,7 +71,6 @@ public class PostAction {
 		PostDAO dao = new PostDAO();
 		List<Post> posts = null;
 
-		//全件取得
 		if (searchWord == null || searchWord.isBlank()) {
 			//全件取得
 			posts = dao.searchPostListByUserId(userId);
@@ -48,11 +81,11 @@ public class PostAction {
 			String term = searchWord.trim();
 
 			if (term.startsWith("#")) {
-				//求人番号検索を行う
+				//求人番号検索
 				posts = dao.searchPostByRecruitmentNo(userId, term.substring(1));
 
 			} else if (term.startsWith("@")) {
-				//試験日検索を行う
+				//試験日検索
 				String prefix = term.substring(1);
 				LocalDate date = LocalDate.parse(prefix, DateTimeFormatter.ofPattern("yyyyMMdd"));
 				posts = dao.searchPostByExamDate(userId, date);
