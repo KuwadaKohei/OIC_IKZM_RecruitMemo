bcbdab702f88e615776c693eeae3c9f1f0247042
KuwadaKohei <24120053@oic-ok.ac.jp>
2025-11-28 14:13:38 +0900

    必要なDAOクラスの作成

---
+++ src/main/java/ikzm_jhm_dao/ExamContentDAO.java
@@ -0,0 +1,14 @@
+package ikzm_jhm_dao;
+
+import java.util.List;
+
+import ikzm_jhm_dto.ExamContent;
+
+public class ExamContentDAO {
+
+	public List<ExamContent> findAll() {
+		// TODO 自動生成されたメソッド・スタブ
+		return null;
+	}
+
+}
