d353c6fa827885e5250144bb7eb026c0dcc54345
KuwadaKohei <24120053@oic-ok.ac.jp>
2025-11-28 12:43:14 +0900

    投稿の詳細を表示するサーブレットの実装

--- src/main/java/ikzm_jhm_servlet/PostDetailViewServlet.java
+++ src/main/java/ikzm_jhm_servlet/PostDetailViewServlet.java
@@ -2,11 +2,15 @@ package ikzm_jhm_servlet;
 
 import java.io.IOException;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+
+import ikzm_jhm_action.PostAction;
+import ikzm_jhm_viewmodel.PostViewModel;
 
 /**
  * 詳細データ取得、権限確認
@@ -15,29 +19,30 @@ import jakarta.servlet.http.HttpServletResponse;
 @WebServlet("/ReportView")
 public class PostDetailViewServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public PostDetailViewServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String strId = request.getParameter("postId");
+		int postId = 0;
+
+		try {
+			postId = Integer.parseInt(strId);
+		} catch (NumberFormatException e) {
+			//
+			response.sendRedirect(request.getContextPath() + "/ReportList");
+			return;
+		}
+
+		PostAction postAction = new PostAction();
+		PostViewModel viewModel = postAction.getPostDetails(postId);
+
+		if (viewModel == null) {
+			//データが存在しない場合
+			request.setAttribute("errorMessage", "指定された投稿は見つかりませんでした。");
+		}
+
+		request.setAttribute("postDetail", viewModel);
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/postView.jsp");
+		dispatcher.forward(request, response);
 	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
-
 }
