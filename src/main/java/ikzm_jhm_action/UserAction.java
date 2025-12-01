ad25e98c18c0d1fdeedcc58dbcb0ca4608c4ac52
KuwadaKohei <24120053@oic-ok.ac.jp>
2025-12-01 13:38:19 +0900

    getDepartmentList()及びregisterUserDetail()の実装

--- src/main/java/ikzm_jhm_action/UserAction.java
+++ src/main/java/ikzm_jhm_action/UserAction.java
@@ -1,8 +1,11 @@
 package ikzm_jhm_action;
 
 import java.util.ArrayList;
+import java.util.List;
 
+import ikzm_jhm_dao.DepartmentDAO;
 import ikzm_jhm_dao.UserDAO;
+import ikzm_jhm_dto.Department;
 import ikzm_jhm_dto.User;
 
 public class UserAction {
@@ -28,12 +31,31 @@ public class UserAction {
 
 	public void banUser(String target) {
 		// TODO 自動生成されたメソッド・スタブ
-		
-	}
 
+	}
 
 	public void unBanUser(String target) {
 		// TODO 自動生成されたメソッド・スタブ
-		
+
+	}
+
+	public List<Department> getDepartmentList() {
+		DepartmentDAO dao = new DepartmentDAO();
+		// DAOが未実装でも動くように、nullチェックや空リスト返却を考慮
+		List<Department> list = dao.findAll();
+		if (list == null) {
+			return new ArrayList<>();
+		}
+		return list;
+	}
+
+	public boolean registerUserDetail(int userId, int departmentId, int grade) {
+		// バリデーション (簡易)
+		if (departmentId <= 0 || grade <= 0) {
+			return false;
+		}
+
+		UserDAO dao = new UserDAO();
+		return dao.updateDepartmentInfo(userId, departmentId, grade);
 	}
 }
