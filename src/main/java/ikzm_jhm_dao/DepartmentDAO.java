6abfb1ea55d4d8d752a35a599a2f690a098c773c
KuwadaKohei <24120053@oic-ok.ac.jp>
2025-12-01 14:58:25 +0900

    DAOメソッドの追加

--- src/main/java/ikzm_jhm_dao/DepartmentDAO.java
+++ src/main/java/ikzm_jhm_dao/DepartmentDAO.java
@@ -1,10 +1,17 @@
 package ikzm_jhm_dao;
 
+import java.util.List;
+
 import ikzm_jhm_dto.Department;
 
 public class DepartmentDAO {
 
 	public Department findById(int departmentId) {
+		// TODO 自動生成されたメソッド・スタブ
+		return null;
+	}
+
+	public List<Department> findAll() {
 		// TODO 自動生成されたメソッド・スタブ
 		return null;
 	}
