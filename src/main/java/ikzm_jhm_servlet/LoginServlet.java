d8e2b27e6b2e9a0038b590c0717ee0f96e1a4e93
KuwadaKohei <24120053@oic-ok.ac.jp>
2025-12-01 18:42:46 +0900

    doPostをdoGetに変更

--- src/main/java/ikzm_jhm_servlet/LoginServlet.java
+++ src/main/java/ikzm_jhm_servlet/LoginServlet.java
@@ -27,7 +27,7 @@ import ikzm_jhm_model.AuthResult;
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
 		String code = request.getParameter("code");
