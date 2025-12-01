53101a8bb63a9025b46596136332682f6e0fa28d
KuwadaKohei <24120053@oic-ok.ac.jp>
2025-12-01 17:11:14 +0900

    エンコーディングフィルターの実装

---
+++ src/main/java/ikzm_jhm_filter/EncodingFilter.java
@@ -0,0 +1,26 @@
+package ikzm_jhm_filter;
+
+import java.io.IOException;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebFilter;
+
+/**
+ * 全リクエストに対してUTF-8エンコーディングを設定するフィルター
+ */
+@WebFilter("/*")
+public class EncodingFilter implements Filter {
+
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+
+		request.setCharacterEncoding("UTF-8");
+		response.setCharacterEncoding("UTF-8");
+
+		chain.doFilter(request, response);
+	}
+}
